e5d40ce892807c46bcc1c0c8d776c904ccb773cc
IreKal <ireneusz.kalwat@10g.pl>
2023-01-15 11:00:44 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    while(true) {
+        int i = 0;
+
+            System.out.println(i + 1);
+            i++;
+        }
+
+
+    }}
